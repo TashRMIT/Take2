accc7c19bff6df5aeb304fab1616fa3e466e16b2
TashRMIT <37699600+TashRMIT@users.noreply.github.com>
2018-10-31 16:22:33 +1100

    Update index.php

--- index.php
+++ index.php
@@ -25,8 +25,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
 	<!-- RA Styles -->
-	<link href="css/w3.css" rel="stylesheet">
+	<!--<link rel="stylesheet" href="css/racss.css" />-->
+	<link href="css/w3.css" rel="stylesheet" />
 	<link rel="stylesheet" href="css/styles.css" />
+	
 
 </head>
 
@@ -46,58 +48,102 @@
 		<h1>Summer Exhibition 2018</h1>
 		<p>Held without interruption since 1769, the Summer Exhibition is the world's oldest open submission exhibtion, displaying works in a variety of mediums and genres by emerging and established artists. In this 250th year, the exhibition will be the biggest in its history.</p>
 		<br />
-		<h3>Art lovers can <a href="#" class="browseButton">Browse all artworks</a></h3>
+		<h3>Art lovers can <button href="#" class="searchButton">Browse all artworks</button></h3>
 		<br />
 		<h3>Or refine your search</h3>
-		<table>
-			<form>
-				<tr>
-					<td colspan="5"><input type="text" class="searchBox" name="nameOfArtwork" placeholder="Explore by artist, artwork or medium" /></td>
-					<td><a href="#" class="browseButton">Search</a></td>
-				</tr>
+		<div id="searchArea">
+			<form class="new_search" id="new_search" action="" accept-charset="UTF-8" method="get">
+				<div class='form-inline searchBox'>
+					<input name="utf8" type="hidden" value="&#x2713;" />
+					<input class="search_form-input filter-search_form-input" placeholder="Search by artist, artwork, medium, catalogue no." type="search" name="search[query]" id="search_query" />
+					<button name="button" type="submit" class="searchButton">Search</button>
+				</div>
+				
+				<div class="form-inline">
+					<label for="search_size" class="sizeLabel">Size</label>
+					<label for="search_type" class="typeLabel">Type</label>
+					<label for="search_gallery" class="galleryLabel">Gallery</label>
+					<label for="search_price_min" class="minLabel">Price min</label>
+					<label for="search_price_max" class="maxLabel">Price max</label>
+				</div>
+				
+				<div class="form-inline">
+					<select name="search[size]" id="search_size">
+						<option value="Any">Any</option>
+						<option value="S">S</option>
+						<option value="M">M</option>
+						<option value="L">L</option>
+						<option value="XL">XL</option>
+					</select>
+
+					<select name="search[type]" id="search_type" class="searchFields">
+						<option value="All">All</option>
+						<option value="Print">Print</option>
+						<option value="Painting">Painting</option>
+						<option value="Architecture">Architecture</option>
+						<option value="Drawing">Drawing</option>
+						<option value="Film">Film</option>
+						<option value="Photography">Photography</option>
+						<option value="Sculpture">Sculpture</option>
+						<option value="Other">Other</option>
+					</select>
+
+					<select name="search[gallery]" id="search_gallery" class="galleryField">
+						<option value="All">All</option>
+						<option value="courtyard-and-staircase">Courtyard and Staircase, Vestibule and Sackler Stairs</option>
+						<option value="wohl-central-hall">Wohl Central Hall</option>
+						<option value="gallery-iii">Gallery III</option>
+						<option value="gallery-iv">Gallery IV</option>
+						<option value="gallery-v">Gallery V</option>
+						<option value="gallery-vi">Gallery VI</option>
+						<option value="gallery-vii">Gallery VII</option>
+						<option value="gallery-viii">Gallery VIII</option>
+						<option value="gallery-ix">Gallery IX</option>
+						<option value="lecture-room">Lecture Room</option>
+						<option value="north-sackler-gallery">North Sackler Gallery</option>
+						<option value="south-sackler-gallery">South Sackler Gallery</option>
+						<option value="west-sackler-gallery">West Sackler Gallery</option>
+						<option value="mcaulay-gallery">Wohl Entrance Hall and McAulay Gallery</option>
+					</select>
+
+					<select name="search[price_min]" id="search_price_min">
+						<option value="No min">No min</option>
+						<option value="£100">£100</option>
+						<option value="£300">£300</option>
+						<option value="£500">£500</option>
+						<option value="£1,000">£1,000</option>
+						<option value="£2,000">£2,000</option>
+						<option value="£3,000">£3,000</option>
+						<option value="£5,000">£5,000</option>
+						<option value="£10,000">£10,000</option>
+						<option value="£50,000">£50,000</option>
+						<option value="£80,000">£80,000</option>
+					</select>	
+
+					<select name="search[price_max]" id="search_price_max">
+						<option value="No max">No max</option>
+						<option value="£100">£100</option>
+						<option value="£300">£300</option>
+						<option value="£500">£500</option>
+						<option value="£1,000">£1,000</option>
+						<option value="£2,000">£2,000</option>
+						<option value="£3,000">£3,000</option>
+						<option value="£5,000">£5,000</option>
+						<option value="£10,000">£10,000</option>
+						<option value="£50,000">£50,000</option>
+						<option value="£80,000">£80,000</option>
+						<option value="£2,000,000">£2,000,000</option>
+						<option value="£350,000,000">£350,000,000</option>
+					</select>
+					<button name="button" type="submit" class="searchButton">Search</button>
+				</div>
+				<div class="form-inline">
+					<input name="search[online_only]" type="hidden" value="0" />
+					<input class="icheck" type="checkbox" value="1" name="search[online_only]" id="search_online_only" />
+					<label for="search_online_only">Only show me works available for sale online</label>
+				</div>
 			</form>
-			<form>
-				<tr>
-					<td>Size</td>
-					<td>Type</td>
-					<td>Gallery</td>
-					<td>Price Min.</td>
-					<td>Price Max.</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>
-						<select>
-							<option>Any</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>No Min.</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>No Max.</option>
-						</select>
-					</td>
-					<td><a href="#" class="browseButton">Search</a></td>
-				</tr>
-				<tr>
-					<td colspan="6"><input type="checkbox" name="saleOnly" /> Only show me works available for sale</td>
-				</tr>
-			</form>
-		</table>
+		</div>
 	</main>
 	<footer id="footer">
         <a href="https://twitter.com/RL_Genius" class="fa fa-twitter-square w3-left-align"></a>
